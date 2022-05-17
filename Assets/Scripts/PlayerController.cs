9d112a2e47f24c163155a4688aa4eb89bc7425ba
Jovon Lim <jovonlim@gmail.com>
2022-05-17 16:53:03 +0800

    Updated Main Scenes with animations and npcs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,8 +33,8 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + 4.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 4.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
