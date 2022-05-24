5923c3f32cd76bd4f5c50840140fddb3717c1c23
Jovon Lim <jovonlim@gmail.com>
2022-05-24 15:20:38 +0800

    Added player melee combos

--- Assets/Scripts/Player/PlayerMelee.cs
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class PlayerMelee : MonoBehaviour
 {
 
@@ -13,25 +14,56 @@ public class PlayerMelee : MonoBehaviour
     [SerializeField] public float attackDamage;
     public LayerMask enemyLayer;
 
+    private int combo = 0;
+    private float resetTimer;
+
+   // public static PlayerMelee instance;
+   // public bool isAttacking = false;
+    
     private void Awake()
     {
         anima = GetComponent<Animator>();
         playerMove = GetComponent<PlayerMovement>();
+        resetTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && attackCooldownTimer > attackSpeed && playerMove.canAttack())
-            Attack();
 
+        if (Input.GetMouseButtonDown(0) && attackCooldownTimer > attackSpeed && playerMove.canAttack())
+        {
+            Attack();
+        }
+      
+        resetTimer += Time.deltaTime;
         attackCooldownTimer += Time.deltaTime;
     }
-
+    
     void Attack()
     {
-        // Play animation
-        anima.SetTrigger("melee");
+        if (resetTimer > 4.0f)
+        {
+            combo = 0;
+        }
+        combo++;
+        if (combo == 1)
+        {
+            anima.SetTrigger("melee1");
+        }
+
+        else if (combo == 2)
+        {
+            anima.SetTrigger("melee2");
+        }
+
+        else if (combo == 3)
+        {
+            anima.SetTrigger("melee3");
+            combo = 0;
+            
+        }
+        resetTimer = 0;
         attackCooldownTimer = 0;
 
         // Detect enemies in range of attack
@@ -44,4 +76,7 @@ public class PlayerMelee : MonoBehaviour
         }
 
     }
+ 
+
+  
 }
