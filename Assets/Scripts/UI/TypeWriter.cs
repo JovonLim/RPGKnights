448d9b9c44a0fee1e53832c16305121ec471b5ba
Jovon Lim <jovonlim@gmail.com>
2022-06-09 22:23:48 +0800

    Added starting dialogue + main menu scene

---
+++ Assets/Scripts/UI/TypeWriter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TypeWriter : MonoBehaviour
+{
+    [SerializeField] private float delay;
+    [SerializeField] private string fullText;
+
+    private void Start()
+    {
+        StartCoroutine(StartText());
+    }
+
+    IEnumerator StartText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            GetComponent<TextMeshProUGUI>().text = fullText.Substring(0, i);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
