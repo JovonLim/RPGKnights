4a079b902bfd03f4e3676e848ac461a809d16d71
JovonLim <jovonlim@gmail.com>
2022-06-14 23:32:13 +0800

    Changed background + added main menu

---
+++ Assets/Scripts/Others/MainMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject NewGame;
+    [SerializeField] GameObject ContinueGame;
+    [SerializeField] GameObject[] menus;
+    bool isSaved;
+    private void Start()
+    {
+        CheckSave();
+    }
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OpenSettings()
+    {
+        menus[0].SetActive(false);
+        menus[1].SetActive(true);
+     
+    }
+
+    public void OpenControls()
+    {
+        menus[1].SetActive(false);
+        menus[2].SetActive(true);
+    }
+
+    public void OpenVolume()
+    {
+        menus[1].SetActive(false);
+        menus[3].SetActive(true);
+    }
+
+    public void Return()
+    {
+        for (int i = 1; i < menus.Length; i++)
+        {
+            menus[i].SetActive(false);
+        }
+        menus[0].SetActive(true);
+        CheckSave();
+
+    }
+   public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void CheckSave()
+    {
+        if (isSaved)
+        {
+            ContinueGame.SetActive(true);
+            NewGame.SetActive(false);
+        }
+        else
+        {
+            ContinueGame.SetActive(false);
+            NewGame.SetActive(true);
+        }
+    }
+}
